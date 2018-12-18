ff2ba68a9dbbff736b0394a7b58a9b87966ed907
Vladislav <vladislav.shvorobuk@gmail.com>
2018-12-18 13:21:06 +0200

    start

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,9 @@
 import MyArray from './index';
 
 describe('Class MyArray', () => {
-  describe('tests for method map', () => {
 
+  describe('tests for method map', () => {
+// 8
     test('if custom context doesn\'t provided, use current context', () => {
       const arr = new MyArray(1,4,0);
       const testArr = [];
@@ -15,8 +16,26 @@ describe('Class MyArray', () => {
       user.testForEach()
       expect(testArr.toString()).toBe('ivan,ivan,ivan');
     });
+// 1
+    test('instance has method map', () => {
+      const arr = new MyArray(1,4,0);
+      expect(arr.map).toBeInstanceOf(Function);
+    });
+// 2
+    test('instance has not Own Property map', () => {
+      const arr = new MyArray(1,4,0);
+      expect(arr.hasOwnProperty('map')).toBeFalsy();
+    });
+// 4
+    test('Method map must return an Array', () => {
+      const arr = new MyArray(1,4,0);
+      expect(arr.map((num) => num + 5).toBeInstanceOf(Array);
+    });
 
   });
 
 
 });
+
+
+//map
