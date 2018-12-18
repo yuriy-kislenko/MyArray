963d94522876dd89bf142b70aa88e451d7fbd480
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2018-12-18 14:13:55 +0200

    method must increment current length by 1, if 1 argument is put

--- src/index.test.js
+++ src/index.test.js
@@ -3,34 +3,39 @@ import MyArray from './index';
 describe('Class MyArray', () => {
   describe('tests for push method', () => {
     test('instance should have method push', () => {
-       const arr = new MyArray(1,2,3);
+      const arr = new MyArray(1, 2, 3);
       expect(new MyArray().push).toBeInstanceOf(Function);
     });
     test('instance has not own property push', () => {
-      const arr = new MyArray(1,2,3);
+      const arr = new MyArray(1, 2, 3);
       expect(arr.hasOwnProperty('push')).toBeFalsy();
-   });
-   test('method returns new length', () => {
-    const arr = new MyArray(1,2,3);
-    expect(arr.push(4, 5, 6)).toEqual(arr.length);
- });
-  });
-/*   describe('tests for method map', () => {
-
-    test('if custom context doesn\'t provided, use current context', () => {
-      const arr = new MyArray(1,4,0);
-      const testArr = [];
-      const user = {
-        name: 'ivan',
-        testForEach () {
-          arr.forEach(() => testArr.push(this.name));
-        }
-      }
-      user.testForEach()
-      expect(testArr.toString()).toBe('ivan,ivan,ivan');
     });
-
-  }); */
+    test('method returns new length', () => {
+      const arr = new MyArray(1, 2, 3);
+      expect(arr.push(4, 5, 6)).toEqual(arr.length);
+    });
+    test('method must increment current length by 1, if 1 argument is put ', () => {
+      const arr = new MyArray(1, 2, 3, 4);
+      arr.push(2);
+      expect(arr.length).toBe(5)
+    });
+  });
+  /*   describe('tests for method map', () => {
+  
+      test('if custom context doesn\'t provided, use current context', () => {
+        const arr = new MyArray(1,4,0);
+        const testArr = [];
+        const user = {
+          name: 'ivan',
+          testForEach () {
+            arr.forEach(() => testArr.push(this.name));
+          }
+        }
+        user.testForEach()
+        expect(testArr.toString()).toBe('ivan,ivan,ivan');
+      });
+  
+    }); */
 
 
 });
