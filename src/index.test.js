09dfa7556e13358fc5863f8db2c9eca5562c13f4
ZelikSV <zeliksv@gamil.com>
2018-12-18 17:02:33 +0300

    Made filter

--- src/index.test.js
+++ src/index.test.js
@@ -1,65 +1,110 @@
-import MyArray from './index';
+import MyArray from "./index";
 
-describe('Class MyArray', () => {
-  describe('tests for method map', () => {
-
-    test('if custom context doesn\'t provided, use current context', () => {
-      const arr = new MyArray(1,4,0);
+describe("Class MyArray", () => {
+  describe("tests for method map", () => {
+    test("if custom context doesn't provided, use current context", () => {
+      const arr = new MyArray(1, 4, 0);
       const testArr = [];
       const user = {
-        name: 'ivan',
-        testForEach () {
+        name: "ivan",
+        testForEach() {
           arr.forEach(() => testArr.push(this.name));
         }
-      }
-      user.testForEach()
-      expect(testArr.toString()).toBe('ivan,ivan,ivan');
+      };
+      user.testForEach();
+      expect(testArr.toString()).toBe("ivan,ivan,ivan");
     });
-
   });
-  describe('tests for method filter', () => {
-    test('instance has method filter', () => {
-      const arr = new MyArray(1,4,0);
+  describe("tests for method filter", () => {
+    test("instance has method filter", () => {
+      const arr = new MyArray(1, 4, 0);
       const testArr = [];
       expect(arr.filter).toBeInstanceOf(Function);
     });
-    test('instance has not Own Property filter', () => {
-      const arr = new MyArray(1,4,0);
+
+    test("instance has not Own Property filter", () => {
+      const arr = new MyArray(1, 4, 0);
       const testArr = [];
-      expect(arr.hasOwnProperty('filter')).toBeFalsy();
+      expect(arr.hasOwnProperty("filter")).toBeFalsy();
     });
-    test('return value of no filter matches, returns empty array', () => {
-      const arr = new MyArray(1,3,4);
-      expect(arr.filter(item=>item==0)).toHaveLength(0);
+
+    test("return value of no filter matches, returns empty array", () => {
+      const arr = new MyArray(1, 3, 4);
+      expect(arr.filter(item => item == 0)).toHaveLength(0);
     });
-    test('expect callback args to be equal 3', () => {
-      var arr = new MyArray(12, 5, 8, 130, 44);
-      function isBigEnough(value) {
-        return value >= 10;
+
+    test("expect callback args to be equal 3", () => {
+      let lengthOfArguments;
+      function callback(x) {
+        lengthOfArguments = arguments.length;
+        return x + 10;
       }
-      var filtered = arr.filter(isBigEnough);
-      expect(filtered.length).toBe(3);
+      new MyArray(1, 10, 20).filter(callback);
+      expect(lengthOfArguments).toBe(3);
     });
 
-    test('method does not mutate initial array', () => {
-      var arr = new Array(12, 5, 8, 130, 44);
-      var cont = String(arr);
-      function isBigEnough(value) {
-        return value >= 0;
-      }
-      arr.filter(isBigEnough);
-      var cont1 = String(arr);
-      expect(cont1 == cont).toBeTruthy();
+    test("method does not mutate initial array", () => {
+      const arr = new MyArray(5, 4, 3);
+      arr.filter(num => num > 0);
+      expect(arr).toEqual(new MyArray(5, 4, 3));
     });
 
-    test('method must return new Array of current elements which are in check condition', () => {
-      var arr = new Array(1,4,4,5,0,0);
-      var filtered = arr.filter((val) => {return val >=4 } );
-      expect(String(filtered)).toBe(String([4,4,5].join(',')));
+    test("method must return new Array of current elements which are in check condition", () => {
+      var arr = new Array(1, 4, 4, 5, 0, 0);
+      var filtered = arr.filter(val => {
+        return val >= 4;
+      });
+      expect(String(filtered)).toBe(String([4, 4, 5].join(",")));
     });
 
+    test("callback has to be a function", () => {
+      let isCallback = false;
+      let callback = x => {
+        isCallback = true;
+        return x + 1;
+      };
+      const arr = new MyArray(1, 10, 20).filter(callback);
+      expect(isCallback).toBe(true);
+    });
 
+    test("returns an empty array when called on empty array", () => {
+      var arr = new Array();
+      var filtered = arr.filter(val => {
+        return val >= 4;
+      });
+      expect(String(filtered)).toBe(String([].join(",")));
+    });
+
+    test("if callback return false filter return empty array", () => {
+      var arr = new Array(1, 2, 4);
+      var filtered = arr.filter(val => {
+        return val <= 0;
+      });
+      expect(String(filtered) === String([])).toBeTruthy();
+    });
+
+    test("if custom context does not provided, use current context", () => {
+      const arr = new MyArray(1, 4, 0);
+      const testArr = [];
+      const user = {
+        name: "ivan",
+        testForEach() {
+          arr.forEach(() => testArr.push(this.name));
+        }
+      };
+      user.testForEach();
+      expect(testArr.toString()).toBe("ivan,ivan,ivan");
+    });
+
+    test("thisArg is set as -this- of callbackFunction properly for filter method", () => {
+      var arr = new Array(2, 6, 10, 12, 16);
+      var checkNumericRange = function(value) {
+        if (typeof value !== "number") return false;
+        else return value >= this.minimum && value <= this.maximum;
+      };
+      var thisArg = { minimum: 10, maximum: 20 };
+      var result = arr.filter(checkNumericRange, thisArg);
+      expect(String(result) == String([10, 12, 16])).toBeTruthy();
+    });
   });
-
-
 });
