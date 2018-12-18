19ef139611aea7fe261b746b8aa6eddebc101985
yurii <yuri.kislenko00@gmail.com>
2018-12-18 16:38:45 +0200

    add beforeEach

--- src/index.test.js
+++ src/index.test.js
@@ -4,24 +4,26 @@ describe('Class MyArray', () => {
 
   describe('Test method forEach of class Array', () => {
 
+    let arr;
+
+    beforeEach(() => {
+        arr = new MyArray(1,4,0);
+    });
+
     test('has method forEach ', () => {
-        const arr = new MyArray();
         expect(arr.forEach).toBeInstanceOf(Function);
     });
 
     test('instance has not Own Property forEach ', () => {
-      const arr = new MyArray();
       expect(arr.hasOwnProperty('forEach')).toBeFalsy();
     });
     
     test('does not mutate initial arr if we do nothing inside the cb ', () => {
-      const arr = new MyArray(1,4,0);
       arr.forEach(a => a);
       expect(arr.toString()).toBe('1,4,0')
     });
 
     test('if custom context doesnt provided, use current context ', () => {
-      const arr = new MyArray(1,4,0);
       const testArr = [];
       const user = {
         name: 'ivan',
@@ -34,33 +36,28 @@ describe('Class MyArray', () => {
     });
 
     test('callback is executed once for each element in the array ', () => {
-      const arr = new MyArray(1, 2, 3, 4);
       const mockCallback = jest.fn(x => 42 + x);
       arr.forEach(mockCallback);
-      expect(mockCallback.mock.calls.length).toBe(4);
+      expect(mockCallback.mock.calls.length).toBe(3);
     });
 
     test('expect callback args to be equal 3', () => {
-      const arr = new MyArray(1);
       const mockCallback = jest.fn( (v, i, arr) => {});
       expect(mockCallback.length).toBe(3);
     });
 
     test('It should pass in the index of each postiion in originalArray as second argument to callback ', () => {
-      const arr = new MyArray(1,2,3);
       const mockCallback = jest.fn( (v, i, arr) =>  i);
       arr.forEach(mockCallback);
       expect(mockCallback.mock.results[0].value).toBe(0);
     });
 
     test('method should return undefined ', () => {
-      const arr = new MyArray(1,2,3);
       const mockCallback = jest.fn( (v, i, arr) =>  i);
       expect(arr.forEach(mockCallback)).toBeUndefined();
     });
 
     test('thisArg is set as this of callbackFunction properly for forEach method ', () => {
-      const arr = new MyArray(1,4,0);
       const testArr = [];
       const user = {
         name: 'ivan',
@@ -78,7 +75,5 @@ describe('Class MyArray', () => {
     });
 
 })
-
-
 
 });
