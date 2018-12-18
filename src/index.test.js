80101ef01b3f592b55267a6ea2d4be2462655505
Kirill.Kovzel <Kirill.Kovzel@gmail.com>
2018-12-18 14:35:04 +0300

    Adds test #1: Class has static method FROM

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,26 @@
-import MyArray from './index';
+import MyArr from "./index";
 
-describe('Class MyArray', () => {
-  describe('tests for method map', () => {
+describe("Class MyArray", () => {
+  // describe('tests for method map', () => {
 
-    test('if custom context doesn\'t provided, use current context', () => {
-      const arr = new MyArray(1,4,0);
-      const testArr = [];
-      const user = {
-        name: 'ivan',
-        testForEach () {
-          arr.forEach(() => testArr.push(this.name));
-        }
-      }
-      user.testForEach()
-      expect(testArr.toString()).toBe('ivan,ivan,ivan');
+  //   test('if custom context doesn\'t provided, use current context', () => {
+  //     const arr = new MyArray(1,4,0);
+  //     const testArr = [];
+  //     const user = {
+  //       name: 'ivan',
+  //       testForEach () {
+  //         arr.forEach(() => testArr.push(this.name));
+  //       }
+  //     }
+  //     user.testForEach()
+  //     expect(testArr.toString()).toBe('ivan,ivan,ivan');
+  //   });
+
+  // });
+
+  describe("tests for method FROM", () => {
+    test("Class has static method FROM", () => {
+      expect(MyArr.from).toBeInstanceOf(Function);
     });
-
   });
-
-
 });
