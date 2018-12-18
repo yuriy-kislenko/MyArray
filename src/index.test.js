e6d93befa6fd8b1a087e5190ca6f727a2fe1274d
Kirill.Kovzel <Kirill.Kovzel@gmail.com>
2018-12-18 18:21:10 +0300

    deletes extra lines

--- src/index.test.js
+++ src/index.test.js
@@ -1,23 +1,6 @@
 import MyArr from "./index";
 
 describe("Class MyArray", () => {
-  // describe('tests for method map', () => {
-
-  //   test('if custom context doesn\'t provided, use current context', () => {
-  //     const arr = new MyArray(1,4,0);
-  //     const testArr = [];
-  //     const user = {
-  //       name: 'ivan',
-  //       testForEach () {
-  //         arr.forEach(() => testArr.push(this.name));
-  //       }
-  //     }
-  //     user.testForEach()
-  //     expect(testArr.toString()).toBe('ivan,ivan,ivan');
-  //   });
-
-  // });
-
   describe("tests for method FROM", () => {
     test("Class has static method FROM", () => {
       expect(MyArr.from).toBeInstanceOf(Function);
