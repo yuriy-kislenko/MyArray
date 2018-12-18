b6674abf0cc1720f94dc8a8ff1bbf4f36b26a772
Lena <elena.borisenko1991@gmail.com>
2018-12-18 13:45:33 +0000

    work correctly with comparator

--- src/index.test.js
+++ src/index.test.js
@@ -29,9 +29,23 @@ describe('Class MyArray', () => {
       const arr = new MyArray(1,4,0);
       expect(arr.hasOwnProperty('sort')).toBeFalsy();
    });
-    test('erorr if comparator is not a function or undefined', () => {
+    /*test('erorr if comparator is not a function or undefined', () => {
       const arr = new MyArray(1,4,0);
-      expect(arr.sort(comparator).type();
+      let comparator = function(a,b){
+        return a-b;
+      }
+
+      expect(comparator).toBeInstanceOf(Function);
+   });*/
+
+   test('should work correctly with comparator', () => {
+      const arr = new MyArray(1,4,0);
+      let comparator = function(a,b){
+        return a-b;
+      }
+
+
+      expect(arr.sort(comparator)).toEqual([0,1,4]);
    });
 
   });
