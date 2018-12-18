08c024ae26a5fd20ce9b2c07db2c3a8d70d71f8b
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2018-12-18 16:44:55 +0200

    add beforeEach

--- src/index.test.js
+++ src/index.test.js
@@ -2,44 +2,49 @@ import MyArray from './index';
 
 describe('Class MyArray', () => {
   describe('tests for push method', () => {
+    let arr;
+    beforeEach(() => {
+      arr = new MyArray(1, 2, 3);
+    });
+
     test('instance should have method push', () => {
-      const arr = new MyArray(1, 2, 3);
       expect(new MyArray().push).toBeInstanceOf(Function);
     });
-    test('instance has not own property push', () => {
-      const arr = new MyArray(1, 2, 3);
+
+    test('instance has not own property push', () => {   
       expect(arr.hasOwnProperty('push')).toBeFalsy();
     });
-    test('method returns new length', () => {
-      const arr = new MyArray(1, 2, 3);
+
+    test('method returns new length', () => {      
       expect(arr.push(4, 5, 6)).toEqual(arr.length);
     });
-    test('method must increment current length by 1, if 1 argument is put ', () => {
-      const arr = new MyArray(1, 2, 3, 4);
+
+    test('method must increment current length by 1, if 1 argument is put ', () => {     
       arr.push(2);
-      expect(arr.length).toBe(5);
-    });
-    test("method doesn't change length, if 0 argument is put", () => {
-      const arr = new MyArray(1, 2, 3, 4);
-      arr.push();
       expect(arr.length).toBe(4);
     });
-    test("pushed element must be the last", () => {
-      const arr = new MyArray(1, 2, 3, 4);
+
+    test("method doesn't change length, if 0 argument is put", () => {    
+      arr.push();
+      expect(arr.length).toBe(3);
+    });
+
+    test("pushed element must be the last", () => {      
       const a = 5;
       arr.push(a);
       expect(arr[arr.length - 1]).toEqual(a);
     });
-    test("should allow to add multiple items", () => {
-      const arr = new MyArray(1, 2, 3, 4);
+
+    test("should allow to add multiple items", () => {    
       expect(arr.push(5, 6, 7)).toEqual(arr.length);
     });
+
     test("push 1 element to an empty array, it`s length must be 1", () => {
       const arr = new MyArray();
       expect(arr.push(1)).toEqual(1);
     });
-    test("returns undefined if there is no such index", () => {
-      const arr = new MyArray(1, 2, 3, 4);
+
+    test("returns undefined if there is no such index", () => {     
       arr.push(5);
       expect(arr[-1]).toBeUndefined();
       expect(arr[arr.length]).toBeUndefined();
