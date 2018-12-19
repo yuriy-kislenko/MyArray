3e7d3280829c62c7dc4748941755ced6cfa0b196
AlexYalovyy <YalovyyAlex@gmail.com>
2018-12-19 14:55:48 +0200

    add checking if arr has method toString

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,21 @@
 import MyArray from './index';
 
 describe('Class MyArray', () => {
-  describe('tests for method map', () => {
 
-    test('if custom context doesn\'t provided, use current context', () => {
-      const arr = new MyArray(1,4,0);
-      const testArr = [];
-      const user = {
-        name: 'ivan',
-        testForEach () {
-          arr.forEach(() => testArr.push(this.name));
-        }
-      }
-      user.testForEach()
-      expect(testArr.toString()).toBe('ivan,ivan,ivan');
+  describe('Tests for method toString', () => {
+
+    let arr;
+
+    beforeEach(() => {
+      arr = new MyArray(1, 2, 3, 4);
     });
 
-  });
+    test('instance has method toString', ()=> {
+      expect(arr.toString).toBeInstanceOf(Function);
+    });
+
+})
+
 
 
 });
