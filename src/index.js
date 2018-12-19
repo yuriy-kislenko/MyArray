d2f848b29b934a91c07079d1d4e66b96545189bb
RuQuentin <Kirill.Kovzel@gmail.com>
2018-12-19 12:55:54 +0300

    Adds checking if MyArray class has own property FROM

--- src/index.js
+++ src/index.js
@@ -1,2 +1,186 @@
-const MyArray = Array;
+// const MyArray = Array;
+
+class MyArray {
+  constructor() {
+    // + добавить проверку на пустое значение - возвращать undefined
+    for (let i = 0, argLength = arguments.length; i < argLength; i += 1) {
+      this[i] = arguments[i];
+    }
+
+    return;
+  }
+
+  get length() {
+    let counter = 0;
+    for (let key in this) {
+      counter = this.hasOwnProperty(key) ? counter + 1 : counter;
+    }
+    return counter;
+  }
+
+  *[Symbol.iterator]() {
+    for (let key in this) {
+      if (this.hasOwnProperty(key)) yield this[key];
+    }
+  }
+}
+
+// ===================== PUSH =====================
+MyArray.prototype.push = function() {
+  let i = 0;
+  for (i = 0, argLength = arguments.length; i < argLength; i += 1) {
+    this[String(this.length)] = arguments[i];
+  }
+  return this.length;
+};
+
+// ===================== POP =====================
+MyArray.prototype.pop = function() {
+  const lastItem = this[String(this.length - 1)];
+  delete this[String(this.length - 1)];
+  return lastItem;
+};
+
+// ===================== FROM =====================
+MyArray.from = function(arrayLike, callback, thisArg) {
+  const newArray = new MyArray();
+
+  if (thisArg) {
+    for (let i = 0, argLength = arrayLike.length; i < argLength; i += 1) {
+      newArray[i] = callback.call(thisArg, arrayLike[i], i, arrayLike);
+    }
+  }
+
+  if (callback && !thisArg) {
+    for (let i = 0, argLength = arrayLike.length; i < argLength; i += 1) {
+      newArray[i] = callback(arrayLike[i], i, arrayLike);
+    }
+  }
+
+  if (!callback && !thisArg) {
+    for (let i = 0, argLength = arrayLike.length; i < argLength; i += 1) {
+      newArray[i] = arrayLike[i];
+    }
+  }
+
+  return newArray;
+};
+
+// ===================== MAP =====================
+MyArray.prototype.map = function(callback, thisArg) {
+  const newArray = new MyArray();
+
+  if (thisArg) {
+    for (let i = 0, argLength = this.length; i < argLength; i += 1) {
+      newArray.push(callback.call(thisArg, this[i], i, this));
+    }
+  } else {
+    for (let i = 0, arrLength = this.length; i < arrLength; i++) {
+      newArray.push(callback(this[i], i, this));
+    }
+  }
+
+  return newArray;
+};
+
+// =================== forEach ===================
+MyArray.prototype.forEach = function(callback, thisArg) {
+  if (thisArg) {
+    for (let i = 0, argLength = this.length; i < argLength; i += 1) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  } else {
+    for (let i = 0, arrLength = this.length; i < arrLength; i++) {
+      callback(this[i], i, this);
+    }
+  }
+  return undefined;
+};
+
+// =================== REDUCE ====================
+MyArray.prototype.reduce = function(callback, initValue) {
+  // + проверка, чтобы аргумент был функцией, и только один
+  // + как поступает оригинальный REDUCE, когда не может обработать элемент
+  let accumulator = initValue ? initValue : 0;
+  const arrLength = this.length;
+  if (arrLength > 0) {
+    for (let i = 0; i < arrLength; i++) {
+      console.log(i);
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+  }
+  return accumulator;
+};
+
+// ===================== FILTER =====================
+MyArray.prototype.filter = function(callback, thisArg) {
+  const newArray = new MyArray();
+
+  if (thisArg) {
+    for (let i = 0, argLength = this.length; i < argLength; i += 1) {
+      if (callback.call(thisArg, this[i], i, this)) newArray.push(this[i]);
+    }
+  } else {
+    for (let i = 0, arrLength = this.length; i < arrLength; i++) {
+      if (callback(this[i], i, this)) newArray.push(this[i]);
+    }
+  }
+
+  return newArray;
+};
+
+// =================== SORT ===================
+MyArray.prototype.sort = function(callback) {
+  let arrLength = this.length;
+  let buffer = this[String(0)];
+
+  switch (callback) {
+    case undefined:
+      for (let j = 0; j < arrLength; j++) {
+        let flag = 0;
+
+        for (let i = 0; i < arrLength - 1; i++) {
+          if (String(this[i]) > String(this[String(i + 1)])) {
+            buffer = this[i];
+            this[i] = this[String(i + 1)];
+            this[String(i + 1)] = buffer;
+            flag++;
+          }
+        }
+        if (flag == 0) break;
+      }
+      return this;
+      break;
+
+    default:
+      for (let j = 0; j < arrLength; j++) {
+        let flag = 0;
+
+        for (let i = 0; i < arrLength - 1; i++) {
+          if (callback(this[i], this[String(i + 1)]) < 0) {
+            buffer = this[i];
+            this[i] = this[String(i + 1)];
+            this[String(i + 1)] = buffer;
+            flag++;
+          }
+        }
+        if (flag == 0) break;
+      }
+      return this;
+  }
+};
+
+// ===================== toString =====================
+MyArray.prototype.toString = function() {
+  const arrLength = this.length;
+  if (arrLength === 0) return "";
+
+  let newString = String(this[String(0)]);
+  for (let i = 1; i < arrLength; i++) {
+    newString = newString + "," + this[i];
+  }
+
+  return newString;
+};
+
 export default MyArray;
--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,20 @@
-import MyArr from "./index";
+import MyArray from "./index";
 
 describe("Class MyArray", () => {
   describe("tests for method FROM", () => {
     test("Class has static method FROM", () => {
-      expect(MyArr.from).toBeInstanceOf(Function);
+      expect(MyArray.from).toBeInstanceOf(Function);
     });
 
     test("Instance has not Own Property FROM", () => {
-      const arr = new MyArr(1, 4, 0);
+      const arr = new MyArray(1, 4, 0);
+      expect(MyArray.hasOwnProperty("from")).toBeTruthy();
       expect(arr.hasOwnProperty("from")).toBeFalsy();
     });
 
     test("If custom context is provided, use its context", () => {
       const objectAside = Object.create({ 0: 2 });
-      const arr = MyArr.from(
+      const arr = MyArray.from(
         [10, 20, 30],
         function(x) {
           return x + this[0];
@@ -23,30 +24,30 @@ describe("Class MyArray", () => {
       expect(String(arr)).toBe("12,22,32");
     });
 
-    test("Method FROM must return new instance of MyArr of current elements", () => {
-      const arr = MyArr.from([10, 20, 30]);
-      expect(arr).toBeInstanceOf(MyArr);
+    test("Method FROM must return new instance of MyArray of current elements", () => {
+      const arr = MyArray.from([10, 20, 30]);
+      expect(arr).toBeInstanceOf(MyArray);
     });
 
     test("Method FROM can include 1, 2 or 3 arguments", () => {
-      const arr1 = MyArr.from([10, 20, 30]);
+      const arr1 = MyArray.from([10, 20, 30]);
 
-      const arr2 = MyArr.from([10, 20, 30], function(x) {
+      const arr2 = MyArray.from([10, 20, 30], function(x) {
         return x + 1;
       });
 
       const objectAside = { 0: 2 };
-      const arr3 = MyArr.from(
+      const arr3 = MyArray.from(
         [10, 20, 30],
         function(x) {
-          return this[0] + 1;
+          return this[0] + x;
         },
         objectAside
       );
 
       expect(String(arr1)).toBe("10,20,30");
       expect(String(arr2)).toBe("11,21,31");
-      expect(String(arr3)).toBe("3,3,3");
+      expect(String(arr3)).toBe("12,22,32");
     });
 
     test("Elements' order in 'a' should be the same as in arrayLike", () => {
