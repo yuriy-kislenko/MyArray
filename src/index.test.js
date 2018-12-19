ea5e8a0a01faaea69ca9652a35162ec0c32cae0f
mneokvsenorm@gmail.com <mneokvsenorm@gmail.com>
2018-12-19 15:32:47 +0200

    reduce method start here3

---
+++ src/index.test.js
@@ -0,0 +1,81 @@
+import MyArray from './index';
+
+describe('Class MyArray', () => {
+  describe('tests for method map', () => {
+
+    test('if custom context doesn\'t provided, use current context', () => {
+      const arr = new MyArray(1,4,0);
+      const testArr = [];
+      const user = {
+        name: 'ivan',
+        testForEach () {
+          arr.forEach(() => testArr.push(this.name));
+        }
+      }
+      user.testForEach()
+      expect(testArr).toEqual(['ivan','ivan','ivan']);
+    });
+
+  });
+
+  describe('tests for method pop', () => {
+    let arr;
+
+    beforeEach(() => {
+      arr = new MyArray(1,4,0)
+    });
+
+    test('instance has method pop', () => {
+      expect(arr.pop).toBeInstanceOf(Function);
+    });
+
+    test('instance has not Own Property pop', () => {
+      expect(arr.hasOwnProperty('pop')).toBeFalsy();
+    });
+
+    test('Method must return deleted element', () => {
+      expect(arr.pop()).toBe(0);
+    });
+
+    test('Method must delete last element from array', () => {
+      const deleted = arr.pop();
+
+      expect(arr[3]).toBeUndefined();
+      expect(deleted).toBe(0);
+    });
+
+    test('Length of array must reduce by 1', () => {
+      arr.pop();
+
+      expect(arr.length).toBe(2);
+    });
+
+    test('must work correctly with empty array', () => {
+      const arrEmpty = new MyArray();
+
+      expect(arrEmpty.pop()).toBeUndefined();
+    });
+
+    test('Empty array length always must be 0', () => {
+      const arrEmpty = new MyArray();
+      const initialLength = arrEmpty.length;
+      arr.pop();
+      arr.pop();
+      const finalLength = arrEmpty.length;
+
+      expect(initialLength).toBe(0);
+      expect(finalLength).toBe(0);
+
+    });
+
+  });
+
+  describe("tests for method reduce", () => {
+    
+    test("instance has method reduce", () => {
+      const arr = new MyArray(1, 4, 0);
+      expect(arr.reduce).toBeInstanceOf(Function);
+    });
+  });
+
+});
