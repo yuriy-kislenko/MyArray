89dc8e01f6e37f6fb5f2b3ea49dbd6d55fc44cfd
Denis Derkach <podstavka128@gmail.com>
2018-12-19 15:43:01 +0200

    instance has method slice

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,47 @@
 import MyArray from './index';
 
 describe('Class MyArray', () => {
-  describe('tests for method map', () => {
 
-    test('if custom context doesn\'t provided, use current context', () => {
-      const arr = new MyArray(1,4,0);
-      const testArr = [];
-      const user = {
-        name: 'ivan',
-        testForEach () {
-          arr.forEach(() => testArr.push(this.name));
-        }
-      }
-      user.testForEach()
-      expect(testArr.toString()).toBe('ivan,ivan,ivan');
+    describe('tests for method map', () => {
+
+        let arr;
+
+        beforeEach(() => {
+            arr = new MyArray(1, 2, 3, 4, 5, 6, 7);
+        });
+
+        test("instance has method slice", () => {
+            expect(arr.slice).toBeInstanceOf(Function);
+        });
+
     });
 
-  });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // describe('tests for method map', () => {
+
+    //     test("should mutate initial array, but not return new one", () => {
+    //         const arr = new MyArray(1, 2, 3, 4); 
+    //         const secondArr = arr;
+    //         secondArr.push(5);
+    //         expect(secondArr).toBe(arr);
+    //     });
+
+    // });
 });
