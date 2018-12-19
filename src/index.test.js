c5ceb3b32fc15024402c3c9a2fa917e2e29394d4
Denis Derkach <podstavka128@gmail.com>
2018-12-19 16:02:49 +0200

    method can include 0, 1 or 2 arguments

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,7 @@ import MyArray from './index';
 
 describe('Class MyArray', () => {
 
-    describe('tests for method map', () => {
+    describe('tests for method slice', () => {
 
         let arr;
 
@@ -21,12 +21,18 @@ describe('Class MyArray', () => {
         test("method must return new array", () => {
             let initialArray = arr;
             expect(arr.slice()).not.toBe(initialArray);
+            expect(arr.slice().toString()).toBe(initialArray.toString());
         });
 
         test("new array must be an instance of My Array", () => {
             expect(arr.slice()).toBeInstanceOf(MyArray);
         });
 
+        test("method can include 0, 1 or 2 arguments", () => {
+            expect(arr.slice().toString()).toEqual('1,2,3,4,5,6,7');
+            expect(arr.slice(1).toString()).toEqual('2,3,4,5,6,7');
+            expect(arr.slice(1,2).toString()).toEqual('2');
+        });
     });
 
 
