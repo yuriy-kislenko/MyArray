d96ffe0ba1ced72b17a0ce4a1021dd9b4fa783e0
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2018-12-20 13:58:33 +0300

    changes comparing arrays one by one elements to comparing arrays

--- src/tests/from.test.js
+++ src/tests/from.test.js
@@ -7,6 +7,7 @@ describe("tests for method FROM", () => {
 
   test("Instance has not Own Property FROM", () => {
     const arr = new MyArray(1, 4, 0);
+
     expect(MyArray.hasOwnProperty("from")).toBeTruthy();
     expect(arr.hasOwnProperty("from")).toBeFalsy();
   });
@@ -15,48 +16,41 @@ describe("tests for method FROM", () => {
     const objectAside = { 0: 2 };
     const arr = MyArray.from(
       [10, 20, 30],
-      function(x) {
+      function (x) {
         return x + this[0];
       },
       objectAside
     );
-    expect(arr[0]).toBe(12);
-    expect(arr[1]).toBe(22);
-    expect(arr[2]).toBe(32);
+
+    expect(arr).toEqual(new MyArray(12, 22, 32));
   });
 
   test("Method FROM must return new instance of MyArray with current elements", () => {
     const arr = MyArray.from([10, 20, 30]);
     const arrControl = { 0: 10, 1: 20, 2: 30 };
+
     expect(arr).toBeInstanceOf(MyArray);
     expect(arr).toEqual(arrControl);
   });
 
   test("Method FROM can include 1, 2 or 3 arguments", () => {
     const arr1 = MyArray.from([10, 20, 30]);
-
-    const arr2 = MyArray.from([10, 20, 30], function(x) {
+    const arr2 = MyArray.from([10, 20, 30], function (x) {
       return x + 1;
     });
-
     const objectAside = { 0: 2 };
     const arr3 = MyArray.from(
       [10, 20, 30],
-      function(x) {
+      function (x) {
         return this[0] + x;
       },
       objectAside
     );
 
-    expect(arr1[0]).toBe(10);
-    expect(arr1[1]).toBe(20);
-    expect(arr1[2]).toBe(30);
-    expect(arr2[0]).toBe(11);
-    expect(arr2[1]).toBe(21);
-    expect(arr2[2]).toBe(31);
-    expect(arr3[0]).toBe(12);
-    expect(arr3[1]).toBe(22);
-    expect(arr3[2]).toBe(32);
+    expect(arr1).toEqual(new MyArray(10, 20, 30));
+    expect(arr2).toEqual(new MyArray(11, 21, 31));
+    expect(arr3).toEqual(new MyArray(12, 22, 32));
+
   });
 
   test("Elements' order in 'a' should be the same as in arrayLike", () => {
