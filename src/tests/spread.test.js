9ffb1e6f47c007538dea1621610c59f98cd7ed0a
Arkant <pan_rostislav@ukr.net>
2018-12-20 13:38:39 +0200

    changed code style

--- src/tests/spread.test.js
+++ src/tests/spread.test.js
@@ -1,22 +1,26 @@
 import MyArray from "../index";
 
-describe('tests for spread', () => {
-    test('instance has method [Symbol.iterator]', () => {
-      const arr = new MyArray(1, 2, 3);
-      expect(arr[Symbol.iterator]).toBeInstanceOf(Function);
-    });
-    
-    test('instance has not own property [Symbol.iterator]', () => {
-      const arr = new MyArray(1, 2, 3);
-      expect(arr.hasOwnProperty("Symbol.iterator")).toBeFalsy();
-    });
 
-    test('array must work with operator spread', () => {
-      const arr2 = new MyArray (1, 4, { name: 'ivan' });
-      const arr1 = new Array(0, ...arr2, 5);
-      expect(arr1[0]).toBe(0);
-      expect(arr1[1]).toBe(1);
-      expect(arr1[2]).toBe(4);
-      expect(arr1[3]).toEqual({ name: 'ivan'});
-    });
+describe('tests for spread', () => {
+  test('instance has method [Symbol.iterator]', () => {
+    const arr = new MyArray(1, 2, 3);
+
+    expect(arr[Symbol.iterator]).toBeInstanceOf(Function);
   });
+  
+  test('instance has not own property [Symbol.iterator]', () => {
+    const arr = new MyArray(1, 2, 3);
+
+    expect(arr.hasOwnProperty('Symbol.iterator')).toBeFalsy();
+  });
+
+  test('array must work with operator spread', () => {
+    const arr2 = new MyArray (1, 4, { name: 'ivan' });
+    const arr1 = new Array(0, ...arr2, 5);
+
+    expect(arr1[0]).toBe(0);
+    expect(arr1[1]).toBe(1);
+    expect(arr1[2]).toBe(4);
+    expect(arr1[3]).toEqual({ name: 'ivan' });
+  });
+});
