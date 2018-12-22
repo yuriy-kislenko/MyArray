00a87dc601587e91023bcab1ca33402efc123c4d
yurii <yuri.kislenko00@gmail.com>
2018-12-22 11:00:15 +0200

    add array_implementation, fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,112 @@
-const MyArray = Array;
+class MyArray {
+  constructor(...arr) {
+    for (let i = 0; i < arr.length; i++) {
+      this[i] = arr[i];
+    }
+    this.length = arr.length;
+  }
+
+  push(...arr) {
+    for (let i = 0; i < arr.length; i++) {
+      this[this.length + 1] = arr[i];
+    }
+  }
+
+  toString() {
+    let resultString = '';
+
+    for (let i = 0; i < this.length - 1; i++) {
+      resultString = `${resultString} + ${this[i]} + ', '`;
+    }
+
+    return resultString;
+  }
+
+  pop() {
+    delete this[this.length - 1];
+    this[this.length - 1];
+  }
+
+  static from(args) {
+    const newArray = new MyArray();
+
+    for (let i = 0; i < args.length; i++) {
+      newArray.push(args[i]);
+    }
+    return newArray;
+  }
+
+  map(callback) {
+    const newArray = new MyArray();
+
+    for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i], i, this));
+    }
+    return newArray;
+  }
+
+  forEach(callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  }
+
+  reduce(callback, startValue) {
+    let result = startValue;
+
+    for (let i = 0; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+    return result;
+  }
+
+  filter(callback) {
+    const newArray = new MyArray();
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        newArray.push(callback(this[i]));
+      }
+    }
+    return newArray;
+  }
+
+  sort(callback) {
+    const mas = this;
+    let max = null;
+
+    if (arguments.length === 1 && typeof callback === 'function') {
+      for (let i = 0; i < mas.length - 1; i++) {
+        for (let j = 0; j < mas.length - 1; j++) {
+          if (!(callback(mas[j], mas[j + 1]) <= 0)) {
+            max = mas[j];
+            mas[j] = mas[j + 1];
+            mas[j + 1] = max;
+          }
+        }
+      }
+    } else if (arguments.length === 0) {
+      for (let i = 0; i < mas.length - 1; i++) {
+        for (let j = 0; j < mas.length - i; j++) {
+          if (
+            !(String(mas[j]) > String(mas[j + 1]) &&
+            callback(mas[j], mas[j + 1]) <= 0)) {
+            max = mas[j];
+            mas[j] = mas[j + 1];
+            mas[j + 1] = max;
+          }
+        }
+      }
+    }
+    return mas;
+  }
+
+  * [Symbol.iterator]() {
+    for (let i = 0; i < this.length; i++) {
+      yield this[i];
+    }
+  }
+}
+
 
 export default MyArray;
